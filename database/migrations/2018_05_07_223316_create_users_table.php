7582f6444e4f57a7ae1b36f830ca1837bc35948d
Matias Vargas <matias.vargasm@usach.cl>
2018-05-09 02:22:29 +0000

    Arreglos Migraciones

--- database/migrations/2018_05_07_223316_create_users_table.php
+++ database/migrations/2018_05_07_223316_create_users_table.php
@@ -15,7 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->unsignedInteger('id_rols');
+            $table->unsignedInteger('id_rol');
             $table->unsignedInteger('id_departamento');
             $table->string('nombre');
             $table->string('apellido');
