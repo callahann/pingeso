3b3684044107b84515c4c32b51a565f420ad812b
Gabriel Fuentes <nunkmeconecto@gmail.com>
2018-05-10 00:42:06 -0300

    Inicio mejorado

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,18 +10,20 @@
 @section('content')   
 </head>
     <body>
-        <div>
+        <div class="container">
             <h1>Sistema de evaluación y calificación de desempeño académico</h1>
             <div>
-                <p>Plataforma de gestión de distribución de tiempo académica, cuyas principales funciones son:</p>
-                <ul>
-                    <li>Crear convenio de académico.</li>
-                    <li>Renegociar convenios.</li>
-                    <li>Revisión de convenios a la finalización del periodo lectivo.</li>
-                    <li>Edición de la evaluación de la distribución de tiempo académica.</li>
-                    <li>Pre-evaluación de convenios.</li>
-                    <li>Justificación sobre las actividades.</li>
-                </ul>
+                <blockquote>
+                    <h3>Plataforma de gestión de distribución de tiempo académica, cuyas principales funciones son:</h3>
+                    <ul>
+                        <li>Crear convenio de académico.</li>
+                        <li>Renegociar convenios.</li>
+                        <li>Revisión de convenios a la finalización del periodo lectivo.</li>
+                        <li>Edición de la evaluación de la distribución de tiempo académica.</li>
+                        <li>Pre-evaluación de convenios.</li>
+                        <li>Justificación sobre las actividades.</li>
+                    </ul>
+                </blockquote>
             </div>
         </div>
     </body>
