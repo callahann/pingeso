808218c562fc112025d123f4cefe43b1f01b18a3
Mario Álvarez Molina <mario.alvarez.m@usach.cl>
2018-05-10 01:47:05 +0000

    Resumen declaraciones, impl POST

--- resources/assets/js/components/formulario/secciones/DatosPersonales.vue
+++ resources/assets/js/components/formulario/secciones/DatosPersonales.vue
@@ -1,68 +1,86 @@
 <template>
     <div>
-        <div class="panel panel-default">
-            <div class="panel-heading panel-title text-center">Datos personales</div>
-            <div class="panel-body">
-                <div class="row">
-                    <div class="form-group col-md-3">
-                        <label for="apellido_paterno">Apellido paterno:</label>
-                        <input type="text" class="form-control" id="apellido_paterno">
-                    </div>
-                    <div class="form-group col-md-3">
-                        <label for="apellido_materno">Apellido materno:</label>
-                        <input type="text" class="form-control" id="apellido_materno">
-                    </div>
-                    <div class="form-group col-md-6">
-                        <label for="nombres">Nombres:</label>
-                        <input type="text" class="form-control" id="nombres">
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="form-group col-md-3">
-                        <label for="facultad">Facultad:</label>
-                        <select class="form-control" id="facultad">
-                            <option>1</option>
-                            <option>2</option>
-                            <option>3</option>
-                            <option>4</option>
-                        </select>
-                    </div>
-                    <div class="form-group col-md-3">
-                        <label for="depto">Departamento:</label>
-                        <select class="form-control" id="depto">
-                            <option>1</option>
-                            <option>2</option>
-                            <option>3</option>
-                            <option>4</option>
-                        </select>
-                    </div>
-                    <div class="form-group col-md-3">
-                        <label for="jerarquia">Jerarquía:</label>
-                        <select class="form-control" id="jerarquia">
-                            <option>1</option>
-                            <option>2</option>
-                            <option>3</option>
-                            <option>4</option>
-                        </select>
-                    </div>
-                    <div class="form-group col-md-3">
-                        <label for="jornada">Jornada:</label>
-                        <select class="form-control" id="jornada">
-                            <option>1</option>
-                            <option>2</option>
-                            <option>3</option>
-                            <option>4</option>
-                        </select>
-                    </div>
-                </div>
+        <div class="row">
+            <div class="form-group col-md-3">
+                <label for="apellido_paterno">Apellido paterno:</label>
+                <input type="text" class="form-control" id="apellido_paterno"
+                    v-model="datosPersonales.apellidoPaterno" :disabled="!editable">
+            </div>
+            <div class="form-group col-md-3">
+                <label for="apellido_materno">Apellido materno:</label>
+                <input type="text" class="form-control" id="apellido_materno"
+                    v-model="datosPersonales.apellidoMaterno" :disabled="!editable">
+            </div>
+            <div class="form-group col-md-6">
+                <label for="nombres">Nombres:</label>
+                <input type="text" class="form-control" id="nombres"
+                    v-model="datosPersonales.nombres" :disabled="!editable">
+            </div>
+        </div>
+        <div class="row">
+            <div class="form-group col-md-3">
+                <label for="facultad">Facultad:</label>
+                <select class="form-control" id="facultad" 
+                    v-model="datosPersonales.facultad" :disabled="!editable">
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                </select>
+            </div>
+            <div class="form-group col-md-3">
+                <label for="depto">Departamento:</label>
+                <select class="form-control" id="depto"
+                    v-model="datosPersonales.departamento" :disabled="!editable">
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                </select>
+            </div>
+            <div class="form-group col-md-3">
+                <label for="jerarquia">Jerarquía:</label>
+                <select class="form-control" id="jerarquia"
+                    v-model="datosPersonales.jerarquia" :disabled="!editable">
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                </select>
+            </div>
+            <div class="form-group col-md-3">
+                <label for="jornada">Jornada:</label>
+                <select class="form-control" id="jornada"
+                    v-model="datosPersonales.jornada" :disabled="!editable">
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                </select>
             </div>
         </div>
     </div>
 </template>
 <script>
     export default {
-        mounted() {
-            console.log('Component mounted.')
+        props: ['editable'],
+        data: function() {
+            return {
+                datosPersonales: {
+                    apellidoPaterno: '',
+                    apellidoMaterno: '',
+                    nombres: '',
+                    facultad: '',
+                    departamento: '',
+                    jerarquia: '',
+                    jornada: ''
+                }
+            }
+        },
+        watch: {
+            datosPersonales: function(newDatosPersonales) {
+                this.$emit('actualizar', newDatosPersonales);
+            }
         }
     }
 </script>
