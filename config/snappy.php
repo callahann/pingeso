e4ecac684d4dbc9a8b4f2ad5d50da7a20e2f0444
Matias Vargas <matias.vargasm@usach.cl>
2018-07-17 02:04:20 +0000

    Inserta numero de paginas en pdf

--- config/snappy.php
+++ config/snappy.php
@@ -7,7 +7,11 @@ return array(
         'enabled' => true,
         'binary'  => '/usr/local/bin/wkhtmltopdf-amd64',
         'timeout' => false,
-        'options' => array(),
+        'options' => array(
+            'footer-center' => 'Pagina [page] de [toPage]',
+            'footer-font-size' => 8,
+            'footer-left' => 'Documento de uso confidencial'
+        ),
         'env'     => array(),
     ),
     'image' => array(
